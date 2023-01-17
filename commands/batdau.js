97d0108c59a11d6989da2184e8e8086971032c5b
Fonk <39910970+qvanphong@users.noreply.github.com>
2023-01-17 23:35:25 +0700

    Update batdau.js

--- commands/batdau.js
+++ commands/batdau.js
@@ -13,6 +13,6 @@ module.exports = async function handle(interaction) {
   await interaction.reply({
     content:
       '<:soyboy2:898561790592548925> Mở bát cược mới, các con bạc đặt cược bằng lệnh `/dat`',
-    files: ['https://media.discordapp.net/attachments/932640939917332540/934361335901347860/unknown.png']
+    files: ['https://cdn.discordapp.com/attachments/955888196632322128/966715527080857601/unknown.png']
     })
 }
